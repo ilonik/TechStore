8f0058e6c9681df79cc7ed2fcc3e9a1ac301c59e
AjshaC <94913424+AjshaC@users.noreply.github.com>
2022-10-28 16:56:47 +0200

    AllDone!

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ function loadProducts() {
     .then(function (products) {
       listOfProducts = products;
       addProductsToWebpage();
-      //myArray = JSON.parse(localStorage.getItem("cart"));
     });
 }
 
@@ -20,9 +19,8 @@ function initSite() {
 function inBasketSite() {
   showCart();
   printlength();
-  //totalPrice();
+
   buybtn.addEventListener("click", () => {
-    //printlength();
     localStorage.clear();
     showCart();
     printlength();
@@ -51,14 +49,7 @@ function addProductsToWebpage() {
   }
 }
 
-//let myArray = [];
-
-// if (!localStorage.getItem("cart")) {
-//   localStorage.setItem("cart", JSON.stringify(myArray));
-// }
-
 function test(e) {
-  // let listfromstorage;
   const product = listOfProducts[e.id];
   if (!localStorage.getItem("cart")) {
     localStorage.setItem("cart", JSON.stringify([product]));
@@ -67,22 +58,7 @@ function test(e) {
     listfromstorage.push(product);
     localStorage.setItem("cart", JSON.stringify(listfromstorage));
   }
-  //myArray.push(product);
 
-  //get LS
-  //listfromstorage.push(product);
-  // myArray.push(product);
-  //console.log(listfromstorage);
-  //send back to ls
-  //localStorage.setItem("cart", JSON.stringify(listfromstorage));
-
-  // var count = document.querySelector(".count");
-  // count.innerHTML = listfromstorage.length;
-  printlength();
-}
-
-function inBasketSite() {
-  showCart();
   printlength();
 }
 
@@ -118,15 +94,9 @@ function showCart() {
 }
 
 function testRemove(e) {
-  //const product = myArray[e.id];
-  //console.log(product)
-
-  //nedan lyste men dne behöver vi ju  ej
   const listfromstorage = JSON.parse(localStorage.getItem("cart"));
   listfromstorage.splice(e, 1);
   localStorage.setItem("cart", JSON.stringify(listfromstorage));
-  //localStorage.setItem("cart", JSON.stringify(myArray));
-  //myArray = JSON.parse(localStorage.getItem("cart"));
 
   showCart();
   printlength();
@@ -173,29 +143,9 @@ function totalPrice() {
 
 //**totalPrice function Ends**/
 
-//load cart product
-// function mycount() {
-//   //let prdcount = getprdcountfromstorage();
-//   if (product.length < 1) {
-//     prdcount = 1;
-//   } else {
-//     prdcount = product[peoducts.length - 1].id;
-//     prdcount++;
-//   }
-
-// }
-// Check your console to see that the products are stored in the listOfProducts varible.
-//console.log(listOfProducts);
-
 //POP-UP VID GENOMFÖRT KÖP. SKA VARA PÅ KUNDVAGNSSIDAN OOOOOBS DENNA ÄR BORTKOMMENDERAD TILLFÄLLIGT FÖR ATT SLIPPA FELMEDDELANDE!!!!!!!!!!!!!!!
 //   const completeYouPurchase = document.querySelector(".completeYouPurchase");
 
 //   completeYouPurchase.addEventListener("click", function () {
 //     alert("Ditt köp är genomfört!");
 //   });
-
-// Add your code here, remember to brake your code in to smaller function blocks
-// to reduce complexity and increase readability. Each function should have
-// an explainetory comment like the one for this function, see row 22.
-
-// TODO: Remove the console.log and these comments when you've read them.
